583a158546e29b4cc5cdcd83a32511fd7471c453
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-22 18:05:17 +0800

    Set up Div For Text Output

--- resources/js/physics_world_init.js
+++ resources/js/physics_world_init.js
@@ -1,6 +1,6 @@
 //This file initializes the physics world for the page
 
-const matterContainer = document.querySelector("#matter-container"); //grab the container div
+const matterContainer = document.querySelector("#matter-container"); //grab the matter 'canvas' div
 var THICCNESS = 60;
 
 // module aliases
@@ -26,42 +26,35 @@ var render = Render.create({
     }
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB]);
-
+//boundary walls
 var ground = Bodies.rectangle(
-matterContainer.clientWidth / 2,
-matterContainer.clientHeight + THICCNESS / 2,
-27184,
-THICCNESS,
-{ isStatic: true }
+    matterContainer.clientWidth / 2,
+    matterContainer.clientHeight + THICCNESS / 2,
+    27184,
+    THICCNESS,
+    { isStatic: true }
 );
 
 let leftWall = Bodies.rectangle(
-0 - THICCNESS / 2,
-matterContainer.clientHeight / 2,
-THICCNESS,
-matterContainer.clientHeight * 5,
-{
-    isStatic: true
-}
+    0 - THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    {
+        isStatic: true
+    }
 );
 
 let rightWall = Bodies.rectangle(
-matterContainer.clientWidth + THICCNESS / 2,
-matterContainer.clientHeight / 2,
-THICCNESS,
-matterContainer.clientHeight * 5,
-{ isStatic: true }
+    matterContainer.clientWidth + THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    { isStatic: true }
 );
 
-// add all of the bodies to the world
+// add booundary walls
 Composite.add(engine.world, [ground, leftWall, rightWall]);
-
 
 // run the renderer
 Render.run(render);
