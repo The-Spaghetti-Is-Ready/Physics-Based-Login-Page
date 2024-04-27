0a19ab8ca27b79aed8049386a139b152c4def4b9
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-27 19:18:03 +0800

    Implement Physical Plinko Machine (No Volume Logic) (#2)
    * Added definition for plinko colliders, added functionality to spawn balls with space bar
    
    * increased plinko colliders and added header into text output div. Added formatting for new Div
    
    * Added Player Movement and Volume Output in the HTML
    
    Changelog:
    - Added arrow key-based movement of the player which sets the position where balls will spawn
    - Refactored ball spawning to account for player position instead of the middle of the viewport
    - Added some comments for rationale
    
    * Fixed Volume Text Output to grab actual value in JS Script
    
    Also added a footer div for volume value labels
    
    * Changed Keybind for Spawning Balls and Added way to Clear all Spawned Balls
    
    * Cleaned up Pegs + Added Comments
    
    Changelog:
    - Added comments for readability (code in general is not very legible lol)
    - Made the pegs smaller and fixed the weird gap to the left of the page
    
    * Added Bucket Colliers and added more peg rows to plinko machine
    
    * Added Additional Movement Keybinds (Arrow Keys) and Fixed Styling for Footer

--- resources/js/physics_world_init.js
+++ resources/js/physics_world_init.js
@@ -1,6 +1,14 @@
+/**
+ * Author: Marco Garzon Lara
+ * Student Number: 33970651
+ * Brief: Unintuitive, physics-based volume mixer UI
+ * Notes: This JavaScript file is so gross and coupled I apologise in advance to whoever has to read this, I wrote this on the fly lol.
+ */
 
-const matterContainer = document.querySelector("#matter-container");
+const matterContainer = document.querySelector("#matter-container"); //Contain the matter world scope/viewport within the matter-container Div.
 const THICCNESS = 60;
+
+let volume = 0;
 
 // module aliases
 var Engine = Matter.Engine,
@@ -9,36 +17,99 @@ var Engine = Matter.Engine,
   Bodies = Matter.Bodies,
   Composite = Matter.Composite;
 
-// create an engine
+// create a physics engine
 var engine = Engine.create();
 
-// create a renderer
+// create a renderer. We are using Matter.js's default 'debug' renderer which limits functionality but saves development time.
 var render = Render.create({
-  element: matterContainer,
-  engine: engine,
-  options: {
-    width: matterContainer.clientWidth,
-    height: matterContainer.clientHeight,
-    background: "transparent",
-    wireframes: true,
-    showAngleIndicator: false
-  }
+    element: matterContainer,
+    engine: engine,
+    options: {
+        width: matterContainer.clientWidth,
+        height: matterContainer.clientHeight,
+        background: "transparent",
+        wireframes: false,
+        showAngleIndicator: false
+    }
 });
 
-// create two boxes and a ground
-// var boxA = Bodies.rectangle(400, 200, 80, 80);
-// var boxB = Bodies.rectangle(450, 50, 80, 80);
-
-for (let i = 0; i < 100; i++) {
-  let circle = Bodies.circle(i, 10, 30, {
-    friction: 0.3,
-    frictionAir: 0.00001,
-    restitution: 0.8
-  });
-  Composite.add(engine.world, circle);
+/**
+ * create plinko machine peg collider grid and buckets
+ */
+for (let i = 0; i < 15; ++i) {
+  let Offset = 25;
+  for(let j = 0; j < 52; ++j) {
+    if(i % 2 == 0) { //if row is even then apply an offset. This displays a plinko-like layout
+      let circle = Bodies.circle((j * 50) + Offset, 250 + i * 50, 8, { //create a circle with the x-pos offset
+          isStatic: true,
+          friction: 0
+      });
+      Composite.add(engine.world, circle); //add the collider onto the scene
+    } else {
+        let circle = Bodies.circle(j * 50, 250 + i * 50, 8, { //create ball without the offset
+            isStatic: true,
+            friction: 0
+        });
+        Composite.add(engine.world, circle);
+    }
+  }
 }
 
-var ground = Bodies.rectangle(
+//Create the plinko buckets
+for(let i = 0; i < 21; ++i) {
+  let currentBucket = Bodies.rectangle(i * (matterContainer.clientWidth / 21), matterContainer.clientHeight - 50, 10, 100, { //Make the bucket
+    isStatic: true,
+    friction: 0
+  });
+  Composite.add(engine.world, currentBucket);
+}
+
+/**
+ * Player particle spawner position and spawning logic
+ */
+var currentPlayerPosition = matterContainer.clientWidth / 2; //Set the inital player position to middle of viewport
+let playerCounter = Bodies.rectangle(currentPlayerPosition, 70, 10, 10, { //Make the player a rectangle
+  isStatic: true
+});
+document.addEventListener('keydown', (e) => { //player movement controls
+  switch(e.key) {
+    case "a": //left
+      currentPlayerPosition += -20;
+      break;
+    case "ArrowLeft":
+      currentPlayerPosition += -20;
+      break;
+    case "d": //right
+      currentPlayerPosition += 20;
+      break;
+    case "ArrowRight":
+      currentPlayerPosition += 20;
+      break;
+  }
+  Matter.Body.set(playerCounter, "position", { x: currentPlayerPosition, y: 70 }); //update player position
+});
+Composite.add(engine.world, playerCounter); //Add player to the world
+
+document.addEventListener('keydown', (e) => {  //Spawns particle when spacebar is pressed.
+    if (e.key === "Enter") { //Spawn a ball when 'b' is pressed into the world
+        //create circle
+        let circle = Bodies.circle(currentPlayerPosition, 80, 10, {
+            friction: 0.3,
+            frictionAir: 0.00001,
+            restitution: 0.8
+        });
+        Composite.add(engine.world, circle); //add ball to physics world
+    }
+    if(e.key === "Escape") {
+      Composite.clear(engine.world, true); //clear all of the spawned balls
+    }
+});
+
+
+/**
+ * Spawn world boundaries
+ */
+var ground = Bodies.rectangle( //ground collision
   matterContainer.clientWidth / 2,
   matterContainer.clientHeight + THICCNESS / 2,
   27184,
@@ -46,7 +117,7 @@ var ground = Bodies.rectangle(
   { isStatic: true }
 );
 
-let leftWall = Bodies.rectangle(
+let leftWall = Bodies.rectangle( //Left collision wall (left browser window border)
   0 - THICCNESS / 2,
   matterContainer.clientHeight / 2,
   THICCNESS,
@@ -56,7 +127,7 @@ let leftWall = Bodies.rectangle(
   }
 );
 
-let rightWall = Bodies.rectangle(
+let rightWall = Bodies.rectangle( //Left collision wall (right browser window border)
   matterContainer.clientWidth + THICCNESS / 2,
   matterContainer.clientHeight / 2,
   THICCNESS,
@@ -64,9 +135,11 @@ let rightWall = Bodies.rectangle(
   { isStatic: true }
 );
 
-// add all of the bodies to the world
-Composite.add(engine.world, [ground, leftWall, rightWall]);
+Composite.add(engine.world, [ground, leftWall, rightWall]); // add all of the bodies to the world
 
+/**
+ * Mouse movement constraints
+ */
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
   mouse: mouse,
@@ -78,10 +151,9 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
   }
 });
 
-Composite.add(engine.world, mouseConstraint);
+Composite.add(engine.world, mouseConstraint); //add this constraint to the world
 
-// allow scroll through the canvas
-mouseConstraint.mouse.element.removeEventListener(
+mouseConstraint.mouse.element.removeEventListener( // allow scroll through the canvas by removing scroll wheel from Composite
   "mousewheel",
   mouseConstraint.mouse.mousewheel
 );
@@ -90,15 +162,18 @@ mouseConstraint.mouse.element.removeEventListener(
   mouseConstraint.mouse.mousewheel
 );
 
-// run the renderer
-Render.run(render);
+/**
+ * Runtime calls
+ */
+Render.run(render); // run the renderer
 
-// create runner
-var runner = Runner.create();
+var runner = Runner.create(); // create runner
 
-// run the engine
-Runner.run(runner, engine);
+Runner.run(runner, engine); // run the engine
 
+/**
+ *  Browser window resizing handlers
+ */
 function handleResize(matterContainer) {
   // set canvas size to new values
   render.canvas.width = matterContainer.clientWidth;
@@ -124,3 +199,5 @@ function handleResize(matterContainer) {
 }
 
 window.addEventListener("resize", () => handleResize(matterContainer));
+
+document.getElementById('output').innerHTML = volume;
